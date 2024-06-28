3d012f6d765e2417fec5578439a92dba7d5a8c85
Luis Carlos <luiscbravo94@gmail.com>
2024-06-28 22:31:38 +0100

    Excercise  done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
+
 const fetchJoke = document.getElementById('fetchJoke')
 const ulContainer = document.getElementById('jokeList')
-const API = 'https://api.chucknorris.io/jokes/random'
 
+const API = `https://api.chucknorris.io/jokes/random`
+//peticion API
 const getJoke = () => {
     fetch(API)
     .then((response) => response.json())
-    .then((joke) => saveJoke(joke))
+    .then((data) => saveJoke(data)) //Aqui invocamos la funcion pasandole la data
     .catch((error) => console.error(error.message))
 }
 
+//Function 
 const saveJoke = (data) => {
-    const joke = localStorage.getItem('joke') // no existe por ahora
+    const joke = localStorage.getItem('joke') // en la primera vuelta no existe, en la segunda si.
     
-    const array = joke ? JSON.parse(joke) : [] // nos queda un array vacio
+    const array = joke ? JSON.parse(joke) : [] // nos queda un array vacio en la primera.
 
     array.push(data) // aqui el array tiene el chiste
 
@@ -20,49 +23,52 @@ const saveJoke = (data) => {
 
     localStorage.setItem('joke', JSON.stringify(array))
     
-    template(localStorage.getItem('joke'))
+    template(localStorage.getItem('joke'))//Invocamos esta funcion y le pasamos los jokes guardados en la variable localStorage
 }
-
+//funcion para crear item list 
 const template = (data) => {
-    const itemsToTemplate = JSON.parse(data)
+    const itemsToTemplate = JSON.parse(data)//Aqui lo convertir data en un array de objetos para recorrerlo.
 
     
     const allListItems = itemsToTemplate.map((item) => {
-        const {value,id} = item
+        const {value,id} = item //usamos en destructiring para sacar el value y id.
         return `<li id=${id} ><h3>${value}</h3><button id=${id} class='button'>Eliminar</button></li>`
     }).join('')
     
 
-    ulContainer.innerHTML = allListItems
+    ulContainer.innerHTML = allListItems //Agregamos allListItems a al ul. contenedor.
 }
 
+//FUNCTION para renderizar los elementos de la variable localStorage, si existe.
 const loadJoke = () =>{
-    const jokesToLoad = localStorage.getItem('joke')
+    const jokesToLoad = localStorage.getItem('joke') // para amacenar los valores que tiene localStorage
     
     jokesToLoad ? template(jokesToLoad) : null
 
    
 }
+
+//Verificar en que boton fue el click y luego pasamos el id a la function removeItem(event)
 ulContainer.addEventListener('click', (event)=>{
     const isClicked = event.target.classList.contains('button')
-    if (isClicked) {
-        removeItem(event.target.id)
-    }
-
+    
+    isClicked ? removeItem(event.target.id) : null
 })
 
+//Function para eliminar el <li>.
 const removeItem = (itemId) => {
-    // console.log(typeof itemId);
-    let array = JSON.parse(localStorage.getItem('joke'))
-    let arrayFiltered = array.filter((item) => item.id !== itemId)
+    let array = JSON.parse(localStorage.getItem('joke')) //Almacenar en el array variable localStorage.
     
-    localStorage.setItem('joke', JSON.stringify(arrayFiltered))
+    let arrayFiltered = array.filter((item) => item.id !== itemId)//Devuelve un array, sin el button ID seleccionado.
+    
+    localStorage.setItem('joke', JSON.stringify(arrayFiltered))//Agrega el arrayFiltered a localStorage.
    
 
-    template(localStorage.getItem('joke'))
+    template(localStorage.getItem('joke'))//Invocamos template(), para renderizar con localStorage actualizado.
 }
 
-fetchJoke.addEventListener('click', getJoke)
-window.addEventListener('load',loadJoke)
+fetchJoke.addEventListener('click', getJoke)//Fetch data a la API
+
+window.addEventListener('load',loadJoke)//Cuando se cargue toda la pagina, me llama a la function loadJoke para renderizar los itemsList 
 
  
